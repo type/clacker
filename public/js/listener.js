fd951c835a6600e2c90bf3669455296ac53fdcad
Shea <shea.ryan@mgo.com>
2014-09-09 19:44:43 -0700

    We have basic timing vectors

--- public/js/listener.js
+++ public/js/listener.js
@@ -17,10 +17,13 @@ $(document).ready(function() {
 
     $('#blah').on('keyup', function(e) {
         keys[findNearest(keys, String.fromCharCode(e.keyCode))].uptime = e.timeStamp;
-        if (testWords(keys, words)) {
+        var wordObj = testWords(keys, words);
+        if (wordObj) {
             // do covariance stuff
-            alert('got it!');
-            console.log(keys);
+            var vector = createTimingVector(findRelevantKeys(keys, wordObj));
+            // now we need to find the relevant keystrokes in the keys array since more than what we need
+            // may have been captured
+            console.log(vector);
             keys = [];
         }
     });
@@ -39,6 +42,23 @@ $(document).ready(function() {
         return _.find(words, function(word) {
             return word.regex.test(curKeys);
         });    
+    }
+
+    function findRelevantKeys(keyArray, word) {
+        // turn the key array into just the keys, put them into a string
+        // find the index in the string of the word
+        // slice out from that index to the length of the word from keyArray
+        var toWord = _.map(keyArray, function(key) {
+            return key.letter
+        }).join('');
+        var start = toWord.substr(0).search(word.regex);
+        return keyArray.slice(start, start + word.word.length);
+    }
+
+    function createTimingVector(sliced) {
+        return _.flatten(_.map(sliced, function(key) {
+            return [key.downtime, key.uptime - key.downtime]
+        }));
     }
 });
 
